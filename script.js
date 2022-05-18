49f54cb5eb036d56ec79faa0d7441162337757b9
Leo <leonelsoto17@gmail.com>
2022-05-18 12:22:56 -0500

    fetching the data asynchrously hooked up to search input

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let apiKey = 'b9138dfaee9c9537cf889f71d76b8645' 
+const searchInput = document.querySelector('#location-search-field')
+const form = document.querySelector('form')
 let getLocation = (location) => {
     return location
 }
@@ -7,7 +9,7 @@ let getWeatherData = async (location) => {
     try {
         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
         let data = await response.json()
-  console.log(data)
+
         return data
     } catch (error) {
         console.log(error)
@@ -22,3 +24,13 @@ let processWeatherData = async (promise) => {
     let weatherDescription = data.weather[0].description;
     return {city, temperature, country, weather, weatherDescription }
 }
+let handleWeatherDataSearch = async () => {
+    let location = searchInput.value
+    let response = await getWeatherData(location)
+    let data = await processWeatherData(response)
+    console.log(data)
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleWeatherDataSearch()
+})
